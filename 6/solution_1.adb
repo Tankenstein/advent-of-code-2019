d404727e022ebb51430e989cda12de40a176cbaa
Uku.Tammet <ukutammet@gmail.com>
2019-12-18 22:31:05 +0200

    Clean up indentation

--- 6/solution_1.adb
+++ 6/solution_1.adb
@@ -7,72 +7,72 @@ with Ada.Strings.Equal_Case_Insensitive;
 with Ada.Containers.Indefinite_Hashed_Maps;
 
 procedure Solution_1 is
-   package IO renames Ada.Text_IO;
-   package Strings renames Ada.Strings;
+    package IO renames Ada.Text_IO;
+    package Strings renames Ada.Strings;
 
-   package Orbits_Maps is new Ada.Containers.Indefinite_Hashed_Maps
-      (Key_Type => String,
-       Element_Type => String,
-       Hash => Ada.Strings.Hash_Case_Insensitive,
-       Equivalent_Keys => Ada.Strings.Equal_Case_Insensitive);
+    package String_Maps is new Ada.Containers.Indefinite_Hashed_Maps
+        (Key_Type => String,
+         Element_Type => String,
+         Hash => Ada.Strings.Hash_Case_Insensitive,
+         Equivalent_Keys => Ada.Strings.Equal_Case_Insensitive);
 
-   Center_Of_Mass : aliased constant String := "COM";
+    Center_Of_Mass : aliased constant String := "COM";
 
-   function Parse_Orbits_From_File (Name: String) return Orbits_Maps.Map is
-      File: IO.File_Type;
-      Orbits: Orbits_Maps.Map;
-   begin
-      IO.Open (File => File, Mode => IO.In_File, Name => Name);
-      While not IO.End_Of_File (File) Loop
-         declare
-            Line : String := IO.Get_Line (File);
-            Paren_Index : Integer := Strings.Fixed.Index(Line, ")", 1);
-            Orbit_Target : String := Line(Line'First..Line'First + Paren_Index - 2);
-            Orbiter : String := Line(Line'First + Paren_Index..Line'Last);
-         begin
-            Orbits.Insert(Orbiter, Orbit_Target);
-         end;
-      end loop;
-      IO.Close (File);
-      return Orbits;
-   end Parse_Orbits_From_File;
+    function Parse_Orbits_From_File (Name: String) return String_Maps.Map is
+        File: IO.File_Type;
+        Orbits: String_Maps.Map;
+    begin
+        IO.Open (File => File, Mode => IO.In_File, Name => Name);
+        While not IO.End_Of_File (File) Loop
+            declare
+                Line : String := IO.Get_Line (File);
+                Paren_Index : Integer := Strings.Fixed.Index(Line, ")", 1);
+                Orbit_Target : String := Line(Line'First..Line'First + Paren_Index - 2);
+                Orbiter : String := Line(Line'First + Paren_Index..Line'Last);
+            begin
+                Orbits.Insert(Orbiter, Orbit_Target);
+            end;
+        end loop;
+        IO.Close (File);
+        return Orbits;
+    end Parse_Orbits_From_File;
 
-   function Path_Length (Orbits: Orbits_Maps.Map; Source: String; Target: String) return Integer is
-      package SU renames Ada.Strings.Unbounded;
-      Steps : Integer := 1;
-      Current_Node : SU.Unbounded_String := SU.To_Unbounded_String (Source);
-   begin
-      while SU.To_String (Current_Node) /= Target Loop
-         Steps := Steps + 1;
-         Current_Node := SU.To_Unbounded_String (Orbits.Element (SU.To_String (Current_Node)));
-      end Loop;
-      return Steps;
-   end Path_Length;
+    function Path_Length (Orbits: String_Maps.Map; Source: String; Target: String) return Integer is
+        package SU renames Ada.Strings.Unbounded;
+        Steps : Integer := 1;
+        Current_Node : SU.Unbounded_String := SU.To_Unbounded_String (Source);
+    begin
+        while SU.To_String (Current_Node) /= Target Loop
+            Steps := Steps + 1;
+            Current_Node := SU.To_Unbounded_String (Orbits.Element (SU.To_String (Current_Node)));
+        end Loop;
+        return Steps;
+    end Path_Length;
 
-   function Count_Orbits (Orbits: Orbits_Maps.Map) return Integer is
-      Orbit_Count : Integer := 0;
-      Orbiter_Cursor : Orbits_Maps.Cursor := Orbits_Maps.First (Orbits);
-   begin
-      while Orbits_Maps.Has_Element (Orbiter_Cursor) Loop
-         declare
-            Current_Orbiter : String := Orbits_Maps.Element (Orbiter_Cursor);
-            Orbits_From_Center : Integer := Path_Length
-               (Orbits => Orbits,
-                Source => Current_Orbiter,
-                Target => Center_Of_Mass);
-         begin
-            Orbit_Count := Orbit_Count + Orbits_From_Center;
-            Orbits_Maps.Next (Orbiter_Cursor);
-         end;
-      end Loop;
-      return Orbit_Count;
-   end Count_Orbits;
+    function Count_Orbits (Orbits: String_Maps.Map) return Integer is
+        Orbit_Count : Integer := 0;
+        Orbiter_Cursor : String_Maps.Cursor := String_Maps.First (Orbits);
+    begin
+        while String_Maps.Has_Element (Orbiter_Cursor) Loop
+            declare
+                Current_Orbiter : String := String_Maps.Element (Orbiter_Cursor);
+                Orbits_From_Center : Integer := Path_Length
+                    (Orbits => Orbits,
+                     Source => Current_Orbiter,
+                     Target => Center_Of_Mass);
+            begin
+                Orbit_Count := Orbit_Count + Orbits_From_Center;
+                String_Maps.Next (Orbiter_Cursor);
+            end;
+        end Loop;
+        return Orbit_Count;
+    end Count_Orbits;
 begin
-   declare
-      Orbits: Orbits_Maps.Map := Parse_Orbits_From_File (Ada.Command_Line.Argument (1));
-      Orbit_Count : Integer := Count_Orbits (Orbits);
-   begin
-      IO.Put_Line (Integer'Image (Orbit_Count));
-   end;
+    declare
+        Orbits: String_Maps.Map := Parse_Orbits_From_File (Ada.Command_Line.Argument (1));
+        Orbit_Count : Integer := Count_Orbits (Orbits);
+    begin
+        IO.Put_Line (Integer'Image (Orbit_Count));
+    end;
 end Solution_1;
 
--- 6/solution_2.adb
+++ 6/solution_2.adb
@@ -8,81 +8,81 @@ with Ada.Containers.Indefinite_Hashed_Maps;
 with Ada.Containers.Indefinite_Vectors;
 
 procedure Solution_2 is
-   package IO renames Ada.Text_IO;
-   package Strings renames Ada.Strings;
+    package IO renames Ada.Text_IO;
+    package Strings renames Ada.Strings;
 
-   package String_Maps is new Ada.Containers.Indefinite_Hashed_Maps
-      (Key_Type => String,
-       Element_Type => String,
-       Hash => Ada.Strings.Hash_Case_Insensitive,
-       Equivalent_Keys => Ada.Strings.Equal_Case_Insensitive);
+    package String_Maps is new Ada.Containers.Indefinite_Hashed_Maps
+        (Key_Type => String,
+         Element_Type => String,
+         Hash => Ada.Strings.Hash_Case_Insensitive,
+         Equivalent_Keys => Ada.Strings.Equal_Case_Insensitive);
 
-   package String_Vectors is new Ada.Containers.Indefinite_Vectors
-      (Index_Type => Natural,
-       Element_Type => String);
+    package String_Vectors is new Ada.Containers.Indefinite_Vectors
+        (Index_Type => Natural,
+         Element_Type => String);
 
-   Center_Of_Mass : aliased constant String := "COM";
-   You : aliased constant String := "YOU";
-   Santa : aliased constant String := "SAN";
-   Not_Found : exception;
+    Center_Of_Mass : aliased constant String := "COM";
+    You : aliased constant String := "YOU";
+    Santa : aliased constant String := "SAN";
+    Not_Found : exception;
 
-   function Parse_Orbits_From_File (Name: String) return String_Maps.Map is
-      File: IO.File_Type;
-      Orbits: String_Maps.Map;
-   begin
-      IO.Open (File => File, Mode => IO.In_File, Name => Name);
-      While not IO.End_Of_File (File) Loop
-         declare
-            Line : String := IO.Get_Line (File);
-            Paren_Index : Integer := Strings.Fixed.Index(Line, ")", 1);
-            Orbit_Target : String := Line(Line'First..Line'First + Paren_Index - 2);
-            Orbiter : String := Line(Line'First + Paren_Index..Line'Last);
-         begin
-            Orbits.Insert(Orbiter, Orbit_Target);
-         end;
-      end loop;
-      IO.Close (File);
-      return Orbits;
-   end Parse_Orbits_From_File;
+    function Parse_Orbits_From_File (Name: String) return String_Maps.Map is
+        File: IO.File_Type;
+        Orbits: String_Maps.Map;
+    begin
+        IO.Open (File => File, Mode => IO.In_File, Name => Name);
+        While not IO.End_Of_File (File) Loop
+            declare
+                Line : String := IO.Get_Line (File);
+                Paren_Index : Integer := Strings.Fixed.Index(Line, ")", 1);
+                Orbit_Target : String := Line(Line'First..Line'First + Paren_Index - 2);
+                Orbiter : String := Line(Line'First + Paren_Index..Line'Last);
+            begin
+                Orbits.Insert(Orbiter, Orbit_Target);
+            end;
+        end loop;
+        IO.Close (File);
+        return Orbits;
+    end Parse_Orbits_From_File;
 
-   function Build_Steps (Orbits: String_Maps.Map; Source: String; Target: String) return String_Vectors.Vector is
-      package SU renames Ada.Strings.Unbounded;
-      Current_Node : SU.Unbounded_String := SU.To_Unbounded_String (Source);
-      Steps : String_Vectors.Vector;
-   begin
-      while SU.To_String (Current_Node) /= Target Loop
-         Current_Node := SU.To_Unbounded_String (Orbits.Element (SU.To_String (Current_Node)));
-         Steps.Append(SU.To_String (Current_Node));
-      end Loop;
-      return Steps;
-   end Build_Steps;
+    function Build_Steps (Orbits: String_Maps.Map; Source: String; Target: String) return String_Vectors.Vector is
+        package SU renames Ada.Strings.Unbounded;
+        Current_Node : SU.Unbounded_String := SU.To_Unbounded_String (Source);
+        Steps : String_Vectors.Vector;
+    begin
+        while SU.To_String (Current_Node) /= Target Loop
+            Current_Node := SU.To_Unbounded_String (Orbits.Element (SU.To_String (Current_Node)));
+            Steps.Append(SU.To_String (Current_Node));
+        end Loop;
+        return Steps;
+    end Build_Steps;
 
-   function Find_First_Intersection (First_Vector: String_Vectors.Vector; Second_Vector: String_Vectors.Vector) return String is
-      Cursor : String_Vectors.Cursor := String_Vectors.First (First_Vector);
-   begin
-      while String_Vectors.Has_Element (Cursor) loop
-         declare
-            Element : String := String_Vectors.Element (Cursor);
-            Index_In_Second : Integer := Second_Vector.Find_Index (Element);
-         begin
-            if Index_In_Second /= String_Vectors.No_Index then
-               return Element;
-            end if;
-            String_Vectors.Next (Cursor);
-         end;
-      end loop;
-      raise Not_Found;
-   end Find_First_Intersection;
+    function Find_First_Intersection (First_Vector: String_Vectors.Vector; Second_Vector: String_Vectors.Vector) return String is
+        Cursor : String_Vectors.Cursor := String_Vectors.First (First_Vector);
+    begin
+        while String_Vectors.Has_Element (Cursor) loop
+            declare
+                Element : String := String_Vectors.Element (Cursor);
+                Index_In_Second : Integer := Second_Vector.Find_Index (Element);
+            begin
+                if Index_In_Second /= String_Vectors.No_Index then
+                    return Element;
+                end if;
+                String_Vectors.Next (Cursor);
+            end;
+        end loop;
+        raise Not_Found;
+    end Find_First_Intersection;
 begin
-   declare
-      Orbits: String_Maps.Map := Parse_Orbits_From_File (Ada.Command_Line.Argument (1));
-      You_Steps : String_Vectors.Vector := Build_Steps (Orbits, You, Center_Of_Mass);
-      Santa_Steps : String_Vectors.Vector := Build_Steps (Orbits, Santa, Center_Of_Mass);
-      Intersection : String := Find_First_Intersection (You_Steps, Santa_Steps);
-      You_Intersection_Index : Integer := You_Steps.Find_Index (Intersection);
-      Santa_Intersection_Index : Integer := Santa_Steps.Find_Index (Intersection);
-   begin
-      IO.Put_Line (Integer'Image (You_Intersection_Index + Santa_Intersection_Index));
-   end;
+    declare
+        Orbits: String_Maps.Map := Parse_Orbits_From_File (Ada.Command_Line.Argument (1));
+        You_Steps : String_Vectors.Vector := Build_Steps (Orbits, You, Center_Of_Mass);
+        Santa_Steps : String_Vectors.Vector := Build_Steps (Orbits, Santa, Center_Of_Mass);
+        Intersection : String := Find_First_Intersection (You_Steps, Santa_Steps);
+        You_Intersection_Index : Integer := You_Steps.Find_Index (Intersection);
+        Santa_Intersection_Index : Integer := Santa_Steps.Find_Index (Intersection);
+    begin
+        IO.Put_Line (Integer'Image (You_Intersection_Index + Santa_Intersection_Index));
+    end;
 end Solution_2;
 
